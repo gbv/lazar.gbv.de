584e996536760e6672b84d09161c58d3c42f75df
Jakob Voss <voss@gbv.de>
2018-03-08 12:59:18 +0100

    Extend API info page

--- README.md
+++ README.md
@@ -24,3 +24,16 @@ Benötigt wird mindestens PHP 7 und composer zur Paketverwaltung.
 
 Das Einstiegsverzeichnis für den Webserver ist `public/`. Für Apache ist eine
 `.htaccess` enthalten.
+
+## Entwicklung
+
+Benötigt PHPUnit und PHP Codesniffer:
+
+    $ composer update
+
+In `Makefile` sind die üblichen Aktionen zusammengefasst:
+
+    $ make style    # PHP-Code aufräumen
+    $ make test     # Unit-Tests
+    $ make web      # Anwendung testweise auf Port 8008 starten
+
--- public/api/index.php
+++ public/api/index.php
@@ -1,17 +1,50 @@
-<?php
-
-require '../../vendor/autoload.php';
-
-?>
-<h1>LaZAR-APIs</h1>
-<p>
-  Dieser Teil der <a href="../">LaZAR-Homepage</a> befindet sich noch im Aufbau!
-</p>
-<ul>
-  <li>
-    <a href="oai">OAI-PMH</a>
-  </li>
-  <li>
-    Linked Open Data
-  </li>
-</ul>
+<!doctype html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <title>LaZAR-APIs</title>
+  </head>
+  <body>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	  <a class="navbar-brand" href="../">LaZAR</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav mr-auto">
+		  <li class="nav-item active">
+			<a class="nav-link" href="#">APIs</a>
+		  </li>
+		</ul>
+	  </div>
+    </nav>
+	<div class="container-fluid">
+		<div class="alert alert-info" role="alert">
+		  Dieser Teil der <a href="../">LaZAR-Homepage</a> befindet sich noch im Aufbau!
+		</div>
+		<h2 id="oai">OAI-PMH</h2>
+		<p>
+		  Unter <code><a href="oai">oai</a></code> steht eine
+		  <a href="http://www.openarchives.org/pmh/">OAI-PMH</a> Schnittstelle zum Abruf aller 
+		  veröffentlichten Datensätze zur Verfügung.
+		</p>
+		<h3>Erweiterungen</h3>
+		<p>
+		  Der OAI-PMH-Endpunkt unterstützt als Nicht-Standard-Erweiterung des OAI-Protokolls 
+		  Kombinationen von sets für Abfragen vom Typ <code>ListRecords</code>. Beispiel:
+		</p>
+		<p>
+		  Beispiele:
+  		  <ul>
+		    <li>
+		  <code><a href="oai?verb=ListRecords&metadataPrefix=easydb&set=pool:1:2,tagfilter:lza"
+			>set=pool:1:2,tagfilter:lza</a></code>
+			</li>
+		</ul>
+		</p>
+	</div>
+    <script src="../js/bootstrap.min.js"></script>
+  </body>
+</html>
