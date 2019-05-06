1c1d174c978c66fc1b2eae68e2ce4bbe4d6c836e
root <root@esx-249.gbv.de>
2019-05-06 09:39:10 +0200

    minor layout fix

--- public/version/header.php
+++ public/version/header.php
@@ -24,4 +24,9 @@
         </ul>
       </div>
     </nav>
+    <div class="splitterUnderNav">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
+      	<path class="lazar-shape-fill" d="M0,6V0h1000v100L0,6z"></path>
+      </svg>
+    </div>
     <div class="container-fluid">
